31d57dc1d0dfcbe60b778ba0e4f67080812f87bf
Felix Hochmeister <fejoho05@gmail.com>
2023-06-24 13:37:53 +0200

    PrintF wird commited

---
+++ JavaIstAuchEineInsel/src/CodeSammlungen/PrintF.java
@@ -0,0 +1,11 @@
+package CodeSammlungen;
+
+public class PrintF {
+
+	public static void main(String[] args) {
+		System.out.printf("%d ist eine Zahl.%n", 200);
+		System.out.printf("\"%s\" ist ein String.%n", "Hallo, ich bin eine Zeichenkette");
+
+	}
+
+}
