3663a404ea1de96e209017c963a5443b3432699c
Felix Hochmeister <fejoho05@gmail.com>
2023-06-26 20:55:54 +0200

    Neue Java Files hinzugefügt und verändert

--- JavaIstAuchEineInsel/src/CodeSammlungen/_003IncrementDecrement.java
+++ JavaIstAuchEineInsel/src/CodeSammlungen/_003IncrementDecrement.java
@@ -16,6 +16,19 @@ public class _003IncrementDecrement {
 		 * Man sieht dass j in zweiten Beispiel zuerst zugewiesen wird und damit bleibt j gleich. erst nachdem j sich selbst neu zugewiesen wird,
 		 * wird es inkrementiert und damit kommt das inkrement nicht zum vorschein.
 		 */
+		
+		//Sleepy opperator -=-
+		j = 10;
+		j -=- j + 3;
+		System.out.println(j);
+		
+		i = 10;
+		i +=- i;
+		System.out.println(i);
+		
+		/*
+		 * anscheinend ist der Verbundopperator vor dem = und danach eine Vorzeichen änderung
+		 */
 	}
 
 }
---
+++ JavaIstAuchEineInsel/src/CodeSammlungen/_004XORTest.java
@@ -0,0 +1,15 @@
+package CodeSammlungen;
+
+public class _004XORTest {
+
+	public static void main(String[] args) {
+		boolean test = true ^ true;
+		System.out.println(test);
+		test = true ^ false;
+		System.out.println(test);
+		test = false ^ false;
+		System.out.println(test);
+
+	}
+
+}
